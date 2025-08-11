78526c72c164be5350ffa17b7d3a8a328f648e6b
Shivangi Singh <Shivangisingh212005@gmail.com>
2025-08-11 15:13:19 +0530

    Update exp2.cpp

--- exp2.cpp
+++ exp2.cpp
@@ -7,18 +7,18 @@ using namespace std;
 void increment(int &S)
 {
     S=S+5000;
-    cout<<"Salary incremented";
+    cout<<"Salary incremented: "<<S<<endl;
 }
 
 int main() {
     int sal=27000;
-    increment(sal);
-    cout << "Value of a is: " << sal << endl;
+  
+    cout << "Value of salary is: " << sal << endl;
+      increment(sal);
     return 0;
 }
 /*Output
-
-Salary incrementedValue of a is: 32000
-
+Value of salary is: 27000
+Salary incremented: 32000
 
 */
