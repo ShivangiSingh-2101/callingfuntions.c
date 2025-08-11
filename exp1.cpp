5faaa25ff4bd0692ceb20caeafc87a1cbee5a915
Shivangi Singh <Shivangisingh212005@gmail.com>
2025-08-11 14:53:55 +0530

    Create exp1.cpp

---
+++ exp1.cpp
@@ -0,0 +1,23 @@
+//Shivangi Singh
+//2407012318
+//B3
+#include <iostream>
+using namespace std;
+
+void swap(int& x, int& y) {
+    int temp = x;
+    x = y;
+    y = temp;
+}
+
+int main() {
+    int a = 5, b = 2;
+    swap(a, b);
+    cout << "Value of a is: " << a << endl;
+    cout << "Value of b is: " << b << endl;
+    return 0;
+}
+/*Output
+Value of a is: 2
+Value of b is: 5
+*/
